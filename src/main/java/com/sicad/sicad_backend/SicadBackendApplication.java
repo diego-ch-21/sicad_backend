3cc4b232048073e3db7b37df5b2cb2378d3a24d1
Diego-ch-21 <158114096+diego-ch-21@users.noreply.github.com>
2025-06-08 20:29:04 -0500

    guardar

---
+++ src/main/java/com/sicad/sicad_backend/SicadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sicad.sicad_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicadBackendApplication.class, args);
+	}
+
+}
