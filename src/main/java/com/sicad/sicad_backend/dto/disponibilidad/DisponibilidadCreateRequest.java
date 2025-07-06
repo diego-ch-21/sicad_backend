78dfa05a8a332bfdac51ea6c090542a098cc7eda
Diego-ch-21 <158114096+diego-ch-21@users.noreply.github.com>
2025-07-06 11:17:38 -0500

    algoritmo

--- src/main/java/com/sicad/sicad_backend/dto/disponibilidad/DisponibilidadCreateRequest.java
+++ src/main/java/com/sicad/sicad_backend/dto/disponibilidad/DisponibilidadCreateRequest.java
@@ -22,7 +22,7 @@ public class DisponibilidadCreateRequest {
     private Integer idCargaElectiva;
 
     @NotBlank(message = "El día de la semana es obligatorio")
-    @Pattern(regexp = "^(lunes|martes|miercoles|jueves|viernes|sabado|domingo)$", message = "Día de la semana inválido")
+    @Pattern(regexp = "^(LUNES|MARTES|MIERCOLES|JUEVES|VIERNES|SABADO|DOMINGO)$", message = "Día de la semana inválido")
     private String diaSemana;
 
     @NotBlank(message = "La hora de inicio es obligatoria")
